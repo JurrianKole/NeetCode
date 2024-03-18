81e389f662b5661f943e95c6a44e47a121d48bf8
Jurrian Kole <jurrian@vicompany.nl>
2024-03-18 16:47:55 +0100

    Add initial Stack roadmap valid parentheses solution

---
+++ NeetCode/Roadmap/Stack.cs
@@ -0,0 +1,39 @@
+﻿namespace NeetCode.Roadmap;
+
+public static class Stack
+{
+    // https://leetcode.com/problems/valid-parentheses/description/
+    public static bool ValidParentheses(string s)
+    {
+        if (s.Length < 2)
+        {
+            return false;
+        }
+        
+        var parentheses = new Dictionary<char, char>
+        {
+            { '(', ')' },
+            { '[', ']' },
+            { '{', '}' }
+        };
+
+        var stack = new Stack<char>();
+
+        foreach (var character in s)
+        {
+            if (parentheses.TryGetValue(character, out var parenthesis))
+            {
+                stack.Push(parenthesis);
+            }
+            else
+            {
+                if (stack.Count == 0 || stack.Pop() != character)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return stack.Count == 0;
+    }
+}
